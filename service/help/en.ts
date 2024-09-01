211a6451dad94b7952bc9c92e6195ca0d395a920
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-09-01 12:59:38 +0400

    Update

---
+++ service/help/en.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024. Selldone® Business OS™
+ *
+ * Author: M.Pajuhaan
+ * Web: https://selldone.com
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ *
+ * All rights reserved. In the weave of time, where traditions and innovations intermingle, this content was crafted.
+ * From the essence of thought, through the corridors of creativity, each word, and sentiment has been molded.
+ * Not just to exist, but to inspire. Like an artist's stroke or a sculptor's chisel, every nuance is deliberate.
+ * Our journey is not just about reaching a destination, but about creating a masterpiece.
+ * Tread carefully, for you're treading on dreams.
+ */
+
+export default {
+    CrossSelling:{
+        Add:{
+            embed:'cm0i1jeq71097w8i42n158rhy',
+            title:'How to add cross selling',
+        }
+    }
+}
