80485157d2519c38622316967f7148e634ffcec8
care4it <92592658+care4it@users.noreply.github.com>
2026-05-15 11:00:18 +0200

    Create de.ts

---
+++ service/internal-pages/de.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2023. Selldone® Business OS™
+ *
+ * Author: M.Pajuhaan
+ * Web: https://selldone.com
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ *
+ * All rights reserved. In the weave of time, where traditions and innovations intermingle, this content was crafted.
+ * From the essence of thought, through the corridors of creativity, each word, and sentiment has been molded.
+ * Not just to exist, but to inspire. Like an artist's stroke or a sculptor's chisel, every nuance is deliberate.
+ * Our journey is not just about reaching a destination, but about creating a masterpiece.
+ * Tread carefully, for you're treading on dreams.
+ */
+
+export default {
+  stories:{
+    title: "Großartige Selldone-Onlineshops zur Inspiration für Unternehmer",
+  },
+
+  /**
+   * @see SupportCountriesPage
+   */
+
+  support_countries:{
+    title:"Entfalten Sie Ihr Potenzial <br />auf dem globalen Marktplatz",
+    subtitle:"Willkommen bei Selldone Business OS – der Zukunft der Commerce-Services. Überwinden Sie die Grenzen traditioneller E-Commerce-Plattformen. Erleben Sie eine Welt ohne Themes, ohne Plugins und ohne Code, der Ihre Kreativität einschränkt. Nutzen Sie die Kraft des visuellen Designs und lassen Sie Ihren Shop wirklich hervorstechen.<br /><b>Erleben Sie Commerce zu 100 % visuell!</b>",
+
+
+   support_countries_and_payments :"Unterstützte Länder und Zahlungen",
+    community_translate: {
+      title: "Verbessern Sie das Erlebnis Ihrer Community",
+      subtitle: "Helfen Sie dabei, die Übersetzung des Business-OS-Dashboards, des Schaufensters und aller Selldone-Komponenten zu verbessern, damit sie für Ihre Community kulturell relevanter und benutzerfreundlicher werden."
+    }
+  },
+
+
+    /**
+     * @see Affiliate_LandingPage
+     */
+    affiliate: {
+        title: "Treten Sie unserem Affiliate-Programm bei",
+        subtitle: "Werden Sie Teil einer spannenden Reise und schaffen Sie sich eine lebenslange passive Einnahmequelle.",
+        lifetime_revenue: {
+            title: "Verdienen Sie lebenslange Einnahmen",
+            subtitle: "Unser Affiliate-Programm ist einfach und lohnend aufgebaut. Teilen Sie Ihren individuellen Link, um neue Nutzer zu Selldone einzuladen, und Sie erhalten eine Provision für jede Transaktion, die diese tätigen – lebenslang. Dazu gehören Einnahmen aus Abonnements, Servicegebühren, Shop-Upgrades und mehr. Es gibt keine Begrenzung dafür, wie viel oder wie lange Sie verdienen können. Treten Sie noch heute bei und verwandeln Sie Ihr Netzwerk in eine stetige Einnahmequelle!"
+        },
+        how_it_works: {
+            title: "So funktioniert es",
+            call_to_action_message: "Verwandeln Sie Ihre Kontakte mit dem Affiliate-Programm von Selldone in ein stetiges Einkommen. Registrieren Sie sich jetzt und verdienen Sie mit!",
+            my_referral_panel: "Mein Empfehlungs-Dashboard"
+        },
+        share_link: {
+            title: "Teilen Sie Ihren individuellen Link",
+            description: "Laden Sie andere mit Ihrem persönlichen Affiliate-Link zu Selldone ein und verdienen Sie mit."
+        },
+        earn: {
+            title: "Verdienen Sie lebenslange Provisionen",
+            description: "Erhalten Sie eine Provision für jede Transaktion Ihrer Empfehlungen, einschließlich Abonnements, Servicegebühren, Shop-Upgrades und mehr."
+        },
+        unlimited: {
+            title: "Unbegrenztes Verdienstpotenzial",
+            description: "Es gibt keine Obergrenze für Ihre Einnahmen und keine zeitliche Begrenzung. Verdienen Sie weiter, solange Ihre Empfehlungen aktiv bleiben."
+        },
+        referee_99_usd_voucher: {
+            title: "Kostenloser 99-$-Gutschein für Empfehlungen",
+            description: "Jeder, der sich über Ihren Link registriert, erhält einen 99-$-Gutschein als Willkommensgeschenk."
+        },
+        tier_update: {
+            title: "Bonus für Tarifstufen-Upgrades",
+            description: "Wenn Sie in höhere Stufen aufsteigen, erhalten Sie höhere Provisionen von allen zuvor von Ihnen empfohlenen Nutzern."
+        }
+    }
+
+
+
+};
