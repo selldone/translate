7d67c5e2a663b2579de2c058b367cbd51403bc91
care4it <92592658+care4it@users.noreply.github.com>
2026-05-15 10:55:52 +0200

    Update de.ts

--- service/guild/de.ts
+++ service/guild/de.ts
@@ -1,25 +1,25 @@
 export default {
   "add_new_member": "Neues Mitglied hinzufügen",
-  "add_new_member_msg": "Soziale Medien, Selldone Shop, Produkt und andere Webadressen...",
-  "member_url_msg": "Geben Sie die neue Mitglieds-URL ein und drücken Sie die Eingabetaste.",
+  "add_new_member_msg": "Soziale Medien, Selldone-Shop, Produkt und andere Webadressen...",
+  "member_url_msg": "Geben Sie die URL des neuen Mitglieds ein und drücken Sie die Eingabetaste.",
   "enable": "Aktivieren (öffentlich)",
-  "enable_msg": "Gilde öffentlich zugänglich?",
+  "enable_msg": "Gilde öffentlich zugänglich machen?",
   "flex": "Flex-Modus",
-  "flex_msg": "Legen Sie die Richtung der Mitglieder fest.",
-  "boarder": "Grenze",
-  "boarder_msg": "Hat Grenze?",
-  "add_guild_action": "Fordern Sie jetzt Ihre Gilde an",
-  "guild_name_plc": "Gib einen eindeutigen Namen für deine Gilde ein... (mindestens 6 Zeichen)",
+  "flex_msg": "Legen Sie die Ausrichtung der Mitglieder fest.",
+  "boarder": "Rahmen",
+  "boarder_msg": "Hat einen Rahmen?",
+  "add_guild_action": "Gilde jetzt beantragen",
+  "guild_name_plc": "Geben Sie einen eindeutigen Namen für Ihre Gilde ein... (mindestens 6 Zeichen)",
   "guild_name": "Gildenname",
-  "born_at": "Geboren in",
+  "born_at": "Gegründet am",
   "notifications": {
-    "create_success": "Deine Gilde wurde erfolgreich aktualisiert.",
+    "create_success": "Ihre Gilde wurde erfolgreich aktualisiert.",
     "member_delete_success": "Mitglied wurde erfolgreich entfernt.",
     "member_update_success": "Mitglied wurde erfolgreich aktualisiert."
   },
   "recommendation_add": {
     "title": "Empfehlung hinzufügen",
-    "message": "Sind Sie sicher, dass Sie dieses Mitglied weiterempfehlen? Ihre Empfehlung wird öffentlich angezeigt.",
+    "message": "Sind Sie sicher, dass Sie dieses Mitglied weiterempfehlen möchten? Ihre Empfehlung wird öffentlich angezeigt.",
     "action": "Ja, Empfehlung hinzufügen"
   },
   "recommendation_remove": {
@@ -29,7 +29,7 @@ export default {
   },
   "delete": {
     "title": "Mitglied löschen",
-    "message": "Möchtest du dieses Mitglied wirklich aus deiner Gilde löschen?",
+    "message": "Möchten Sie dieses Mitglied wirklich aus Ihrer Gilde löschen?",
     "action": "Ja, jetzt löschen"
   }
 };
