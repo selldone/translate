349c7ef3cb76d16b272e538d5c2b8906c6037883
care4it <92592658+care4it@users.noreply.github.com>
2026-05-15 09:28:11 +0200

    Fix German translation for accounting.php

--- backend/de/accounting.php
+++ backend/de/accounting.php
@@ -1,11 +1,11 @@
 <?php 
 
  return [
-     "RECEIPT_ACCOUNT_FEE" => "Kontogebühr erstellen für Konto: :Kontonummer",
+     "RECEIPT_ACCOUNT_FEE" => "Kontogebühr für Konto erstellen: :Kontonummer",
      "GATEWAY_NOT_ACTIVE_IN_SYSTEM" => "Gateway im System nicht aktiv.",
-     "GATEWAY_CURRENCY_NOT_SUPPORT" => "Die Währung wurde vom Gateway nicht unterstützt.",
-     "TRANSACTION_CREATE_ERROR" => "Beim Zahlungseingang ist ein Fehler aufgetreten.",
+     "GATEWAY_CURRENCY_NOT_SUPPORT" => "Die Währung wird vom Gateway nicht unterstützt.",
+     "TRANSACTION_CREATE_ERROR" => "Beim Erstellen der Transaktion ist ein Fehler aufgetreten.",
      "GATEWAY_NOT_FOUND" => "Gateway nicht gefunden.",
-     "SHOP_GATEWAY_MISS_CONFIG" => "Die Zahlungseinstellungen für den Shop sind falsch.",
-     "TRANSACTION_NEED_USER_ERROR" => "Benutzer muss zum Erstellen dieser Transaktion festgelegt werden.",
-];
+     "SHOP_GATEWAY_MISS_CONFIG" => "Die Zahlungseinstellungen für den Shop sind fehlerhaft konfiguriert.",
+     "TRANSACTION_NEED_USER_ERROR" => "Es muss ein Benutzer angegeben werden, um diese Transaktion zu erstellen.",
+ ];
